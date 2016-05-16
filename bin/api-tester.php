c4f473029f074eb0c8d2fd0af6d96e6932475fb0
aa <andrey.astakhov@gmail.com>
2016-05-16 23:10:24 +0200

    Implemented support of different locations of autoload.php

--- bin/api-tester.php
+++ bin/api-tester.php
@@ -1,13 +1,19 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+$autoloadPaths = [__DIR__.'/../vendor/autoload.php', __DIR__ . '/../../../autoload.php'];
+foreach ($autoloadPaths as $autoloadPath) {
+    if (file_exists($autoloadPath)) {
+        require $autoloadPath;
+
+        break;
+    }
+}
 
 use Aa\ApiTester\ApiTest\Runner;
 use Aa\ApiTester\ApiTest\SuiteLoader;
 
-
 $suiteLoader = new SuiteLoader();
-$suite = $suiteLoader->loadFromDir(__DIR__.'/../fixtures');
+$suite = $suiteLoader->loadFromDir($argv[1]);
 
 $runner = new Runner();
 $runner->run($suite);
