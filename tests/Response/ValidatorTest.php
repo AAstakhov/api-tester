f6ce08e9d83944ee4c2fb40cdd0559945057cd45
aa <andrey.astakhov@gmail.com>
2016-05-19 21:31:10 +0200

    Added unit test for failure case

--- tests/Response/ValidatorTest.php
+++ tests/Response/ValidatorTest.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints\NotNull;
 
 class ValidatorTest extends PHPUnit_Framework_TestCase
 {
-    public function testValidateReturnsNoViolationMessages()
+    public function testValidateReturnsNoViolations()
     {
         $validator = new Validator();
 
@@ -27,8 +27,31 @@ class ValidatorTest extends PHPUnit_Framework_TestCase
             'body/height' => new GreaterThan(['value' => '100']),
         ];
 
-        $violationMessages = $validator->validate($response, $constraints);
+        $violations = $validator->validate($response, $constraints);
 
-        $this->assertCount(0, $violationMessages);
+        $this->assertCount(0, $violations);
+    }
+
+    public function testValidateReturnsViolations()
+    {
+        $validator = new Validator();
+
+        $body = [
+            'name' => 'Bilbo',
+            'height' => '99',
+        ];
+
+        $response = new Response(451, [], json_encode($body));
+        $constraints = [
+            'status_code' => new EqualTo(['value' => 451]),
+            'body/name' => new NotNull(),
+            'body/height' => new GreaterThan(['value' => '100']),
+        ];
+
+        $violations = $validator->validate($response, $constraints);
+
+        $this->assertCount(1, $violations);
+        $this->assertInstanceOf('\Symfony\Component\Validator\ConstraintViolationInterface', $violations[0]);
+        $this->assertEquals('body/height', $violations[0]->getPropertyPath());
     }
 }
